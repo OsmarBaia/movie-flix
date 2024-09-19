12c620940bd3854c8d971b01cdac20428f5ca73c
Osmar Baia <osmarneto.23@gmail.com>
2024-09-18 22:02:27 -0300

    Menu Toggle and minor tweaks

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
-
-
-const toggleNavMenu = document.querySelector(".toggle-nav");
-const toggleNavMenuIcon = document.querySelector(".toggle-nav i");
+// Containers
 const navMenuContainer = document.querySelector("nav");
+const toggleNavMenuBgBlur = document.querySelector(".bg-blur");
+// Toggles
+const toggleNavMenu = document.querySelector(".nav-toggle");
+const toggleNavMenuIcon = document.querySelector(".nav-toggle i");
+const navCloseToggles = document.querySelectorAll(".nav-close");
 
-function toggleMenu() {
-    navMenuContainer.classList.toggle("active");
-    toggleNavMenuIcon.classList.toggle("bx-menu");
-    toggleNavMenuIcon.classList.toggle("bx-x");
+function OpenNavMenu(){
+    if(!navMenuContainer.classList.contains("active")){
+        toggleNavMenuIcon.classList.remove("bx-menu");
+        toggleNavMenuIcon.classList.add("bx-x");
+        navMenuContainer.classList.add("active");
+        toggleNavMenuBgBlur.classList.add("active");
+    }
 }
 
-toggleNavMenu.addEventListener("click", toggleMenu);
+function CloseNavMenu(){
+    if(navMenuContainer.classList.contains("active")){
+        toggleNavMenuIcon.classList.remove("bx-x");
+        toggleNavMenuIcon.classList.add("bx-menu");
+        navMenuContainer.classList.remove("active");
+        toggleNavMenuBgBlur.classList.remove("active");
+    }
+}
+
+function ToggleNavMenu(){
+    !navMenuContainer.classList.contains("active") ?   OpenNavMenu():  CloseNavMenu();
+}
+
+toggleNavMenu.addEventListener("click",ToggleNavMenu);
+navCloseToggles.forEach(item => item.addEventListener("click", CloseNavMenu));
